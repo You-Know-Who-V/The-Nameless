e8964dde67f1add0da547ecf57d056601b246ec6
You-Know-Who-V <rajgondalia123@gmail.com>
2020-07-07 12:55:27 +0530

    stupid emulator

--- app/src/main/java/com/example/thenameless/SignUpActivity.java
+++ app/src/main/java/com/example/thenameless/SignUpActivity.java
@@ -187,7 +187,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                                 }
                             });
 
-                            startActivity(new Intent(SignUpActivity.this, HomePage.class));
+                            //startActivity(new Intent(SignUpActivity.this, HomePage.class));
                             //finish();
                         }
                     }
