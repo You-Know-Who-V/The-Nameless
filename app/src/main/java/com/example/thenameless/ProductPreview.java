986ad570e44742c3c9816c8f3fc3a9407e626bf6
HrujulThumar22 <46757268+HrujulThumar22@users.noreply.github.com>
2020-07-18 15:40:37 +0530

    Account Details Pop Up Added
    Signed-off-by: HrujulThumar22 <46757268+HrujulThumar22@users.noreply.github.com>

--- app/src/main/java/com/example/thenameless/ProductPreview.java
+++ app/src/main/java/com/example/thenameless/ProductPreview.java
@@ -3,7 +3,10 @@ package com.example.thenameless;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -14,17 +17,25 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.thenameless.model.Namelesser;
 import com.example.thenameless.model.ProductDetails;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Document;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,10 +47,14 @@ public class ProductPreview extends AppCompatActivity implements View.OnClickLis
     private Bundle bundle;
 
     private TextView priceTextView, detailsTextView, userNameTextView, titleTextView;
+    TextView nameTextView,email,phno;
+    ImageView profileImageView;
+    Dialog myDailog;
     private Button callButton, chatButton;
     private ImageButton previousImage, nextImage, favButton;
     private ImageView imageView;
 
+    private FirebaseUser currentUser;
     private int currentImageIndex = 0, mx = 1, flag = 0;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -105,7 +120,7 @@ public class ProductPreview extends AppCompatActivity implements View.OnClickLis
                         }
                     }
                 });
-
+        userNameTextView.setOnClickListener(this);
         nextImage.setOnClickListener(this);
         previousImage.setOnClickListener(this);
         callButton.setOnClickListener(this);
@@ -114,6 +129,51 @@ public class ProductPreview extends AppCompatActivity implements View.OnClickLis
 
     }
 
+    public void getDetails()
+    {
+        db.collection("AccountDetails").whereEqualTo("Name",bundle.getString("userName"))
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful())
+                        {
+                            myDailog.setContentView(R.layout.custom_pop_up);
+                            nameTextView=myDailog.findViewById(R.id.name);
+                            email=myDailog.findViewById(R.id.email);
+                            phno=myDailog.findViewById(R.id.phno);
+                            profileImageView=myDailog.findViewById(R.id.profilepic);
+                            ImageView close=myDailog.findViewById(R.id.txtclose);
+                            close.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    myDailog.dismiss();
+                                }
+                            });
+                            for(QueryDocumentSnapshot document: task.getResult())
+                            {
+                                Log.d("Info", String.valueOf(document.getData()));
+                                Map<String, Object> info=document.getData();
+                                nameTextView.setText(info.get("Name").toString());
+                                phno.setText(info.get("PhoneNo").toString());
+                                email.setText(info.get("EMail").toString());
+                                Glide.with(ProductPreview.this)
+                                        .load(info.get("ProfileImg"))
+                                        .into(profileImageView);
+                            }
+                            myDailog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                            myDailog.show();
+
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ProductPreview.this,"Unable to retrieve Information  :(",Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.details_menu_bar,menu);
@@ -183,6 +243,12 @@ public class ProductPreview extends AppCompatActivity implements View.OnClickLis
                 else {
                     removeFavourite();
                 }
+            case R.id.preview_account_textView:
+            {
+                myDailog=new Dialog(ProductPreview.this);
+
+                getDetails();
+            }
         }
 
     }
